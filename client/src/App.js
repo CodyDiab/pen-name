01fdd908c14e044dd960393a285b245561f6e5e5
Diab <emailcodydiab@gmail.com>
2020-08-17 07:26:25 -0500

    set up file structure

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'semantic-ui-less/semantic.less';
+import Login from './pages/Login';
+import Sigmup from './pages/Signup';
+
+
+function App() {
+  return (
+    <>
+    <Login></Login>
+    <Sigmup></Sigmup>
+    </>
+  );
+}
+
+export default App;
---
+++ server/config/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+//need to set up credentials 
+const connection = "mongodb+srv://username:<password>@<cluster>/<database>?retryWrites=true&w=majority";
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
+
+    module.exports = mongoose.connection;
