cb64fe51ca26995536febdde202861cb61d547e5
Diab <emailcodydiab@gmail.com>
2020-08-18 19:44:28 -0500

    test npm

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//mongo "mongodb+srv://cluster0.z3tav.gcp.mongodb.net/<dbname>" --username Pen-Name-db
 //need to set up credentials 
 const connection = "mongodb+srv://username:<password>@<cluster>/<database>?retryWrites=true&w=majority";
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
