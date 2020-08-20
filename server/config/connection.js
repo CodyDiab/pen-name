8c9e4fb70a08a608f670e4d8fd67dc57212f8454
Diab <emailcodydiab@gmail.com>
2020-08-20 07:40:30 -0500

    fix db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/pen-name',
+  process.env.MONGODB_URI || 'mongodb://localhost/Pen-Name',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
