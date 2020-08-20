92ea1f34f1dcdfcacd74d7ca48e7d194644e9c9a
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-08-20 10:24:07 -0500

    update resolvers/typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,51 +4,53 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
 
-  type Post {
-    _id: ID
-    postText: String
-    createdAt: String
-    username: String
-    commentCount: Int
-    comments: [Comment]
-  }
+    type Post {
+        _id: ID
+        postText: String
+        createdAt: String
+        username: String
+        commentCount: Int
+        comments: [Comment]
+    }
 
-  type Comment {
-    _id: ID
-    commentBody: String
-    createdAt: String
-    username: String
-  }
+    type Comment {
+        _id: ID
+        commentBody: String
+        createdAt: String
+        username: String
+    }
 
-  type User {
-    _id: ID
-    username: String
-    email: String
-    followerCount: Int
-    posts: [Post]
-    followers: [User]
-  }
+    type User {
+        _id: ID
+        username: String
+        about: String
+        linkToPortfolio: String
+        email: String
+        followerCount: Int
+        posts: [Post]
+        followers: [User]
+    }
 
-  type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-    posts(username: String): [Post]
-    post(_id: ID!): Post
-  }
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        posts(username: String): [Post]
+        post(_id: ID!): Post
+    }
 
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    addPost(postText: String!): Post
-    addComment(postId: ID!, commentBody: String!): Post
-    addFollower(followerId: ID!): User
-  }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addPost(postText: String!): Post
+        addComment(postId: ID!, commentBody: String!): Post
+        addFollower(followerId: ID!): User
+    }
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+    type Auth {
+        token: ID!
+        user: User
+    }
 `;
 
 // export the typeDefs
