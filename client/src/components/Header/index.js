2df22ff1171fe8dc3417ff133978ef92132be607
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-08-24 17:30:32 -0500

    update navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -20,6 +20,121 @@ const Header = () => {
   };
 
   
+ function toggleNav() {
+    dispatch({type: TOGGLE_NAV});
+}
+ 
+      
+  
+ if (!state.navActive){
+    return(
+      <nav className="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link to="/" className="">
+        <img className="graphic"src={require("../../resources/PenNameGraphic.svg")}></img>
+          {/* <h1 className="main-title">Pen Name</h1>*/ }
+          </Link>
+        
+          <a role="button" onClick={toggleNav} className="navbar-burger" id="" aria-label="menu" aria-expanded="false" data-target="navMenu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+       </div>
+       <div id="navMenu" className="navbar-menu"> 
+        <div className="navbar-start">
+          {Auth.loggedIn() ? (
+            <>
+            
+            <Link className="navbar-item" to="/profile" onClick={toggleNav}>Profile</Link>
+            <Link className="navbar-item" to="/write" onClick={toggleNav}>Write</Link>
+            <Link className="navbar-item" to="/" onClick={logout}>
+             Logout
+            </Link>
+            </>
+          ):(  <>
+            <Link  className="navbar-item" to="/login" onClick={toggleNav}>Login</Link>
+            <Link  className="navbar-item" to="/signup" onClick={toggleNav}>Signup</Link>
+          </>)}
+        </div>
+        <div className="navbar-end"></div>
+        </div>
+      </nav>
+      )
+    } else {
+      
+    return (
+      <nav className="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link to="/" className="">
+        <img className="graphic"src={require("../../resources/PenNameGraphic.svg")}></img>
+        </Link>
+        
+        <a role="button" onClick={toggleNav} className="navbar-burger is-active" id="" aria-label="menu" aria-expanded="false" data-target="navMenu">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+     </div>
+    
+  
+       <div id="navMenu" className="navbar-menu is-active "> 
+        <div className="navbar-start">
+          {Auth.loggedIn() ? (
+            <>
+            <span>Logged in as {userData.me.username}</span>
+            <Link className="navbar-item" to="/profile" onClick={toggleNav}>Profile</Link>
+            <Link className="navbar-item" to="/write" onClick={toggleNav}>Write</Link>
+            <Link className="navbar-item" to="/" onClick={logout}>
+             Logout
+            </Link>
+            </>
+          ):(  <>
+            <Link  className="navbar-item" to="/login" onClick={toggleNav}>Login</Link>
+            <Link  className="navbar-item" to="/signup" onClick={toggleNav}>Signup</Link>
+          </>)}
+        </div>
+        <div className="navbar-end"></div>
+        </div>
+  
+         
+  
+        </nav>
+       ) 
+        
+        
+    
+   }
+  };
+  
+  export default Header;
+
+
+
+
+  /*
+  import React,{useEffect} from 'react';
+import {TOGGLE_NAV} from '../../utils/actions';
+import Auth from '../../utils/auth';
+import { useStoreContext } from '../../utils/GlobalState';
+import {QUERY_ME_BASIC} from '../../utils/queries';
+import { useQuery } from '@apollo/react-hooks';
+
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+  const { data:userData } = useQuery(QUERY_ME_BASIC);
+  console.log(userData)
+
+  const [state, dispatch] = useStoreContext();
+
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  
   function toggleNav() {
     dispatch({type: TOGGLE_NAV});
 }
@@ -32,62 +147,62 @@ const Header = () => {
       <div className="navbar-brand">
         <Link to="/" className="">
         <img className="graphic"src={require("../../resources/PenNameGraphic.svg")}></img>
-          {/* <h1 className="main-title">Pen Name</h1> */}
+          {// <h1 className="main-title">Pen Name</h1> //}
+          </Link>
+        
+          <a role="button" onClick={toggleNav} className="navbar-burger" id="stick" aria-label="menu" aria-expanded="false" >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+       </div>
+       
+      </nav>
+      )
+    } else {
+      
+    return (
+      <nav className="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">
+        <img className="graphic"src={require("../../resources/PenNameGraphic.svg")}></img>
         </Link>
         
-        <a role="button" onClick={toggleNav} className="navbar-burger" id="stick" aria-label="menu" aria-expanded="false" >
+        <a role="button" onClick={toggleNav} className="navbar-burger is-active" id="stick" aria-label="menu" aria-expanded="false" >
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
      </div>
-     
-    </nav>
-    )
-  } else {
     
-  return (
-    <nav className="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <Link to="/" className="navbar-item">
-      <img className="graphic"src={require("../../resources/PenNameGraphic.svg")}></img>
-      </Link>
-      
-      <a role="button" onClick={toggleNav} className="navbar-burger is-active" id="stick" aria-label="menu" aria-expanded="false" >
-    <span aria-hidden="true"></span>
-    <span aria-hidden="true"></span>
-    <span aria-hidden="true"></span>
-  </a>
-   </div>
   
-
-     <div className="navbar-menu is-active "> 
-      <div className="navbar-start">
-        {Auth.loggedIn() ? (
-          <>
-          <span>Logged in as {userData.me.username}</span>
-          <Link className="navbar-item" to="/profile" onClick={toggleNav}>Profile</Link>
-          <Link className="navbar-item" to="/write" onClick={toggleNav}>Write</Link>
-          <Link className="navbar-item" to="/" onClick={logout}>
-           Logout
-          </Link>
-          </>
-        ):(  <>
-          <Link  className="navbar-item" to="/login" onClick={toggleNav}>Login</Link>
-          <Link  className="navbar-item" to="/signup" onClick={toggleNav}>Signup</Link>
-        </>)}
-      </div>
-      <div className="navbar-end"></div>
-      </div>
-
-       
-
-      </nav>
-     ) 
-      
-      
+       <div className="navbar-menu is-active "> 
+        <div className="navbar-start">
+          {Auth.loggedIn() ? (
+            <>
+            <span>Logged in as {userData.me.username}</span>
+            <Link className="navbar-item" to="/profile" onClick={toggleNav}>Profile</Link>
+            <Link className="navbar-item" to="/write" onClick={toggleNav}>Write</Link>
+            <Link className="navbar-item" to="/" onClick={logout}>
+             Logout
+            </Link>
+            </>
+          ):(  <>
+            <Link  className="navbar-item" to="/login" onClick={toggleNav}>Login</Link>
+            <Link  className="navbar-item" to="/signup" onClick={toggleNav}>Signup</Link>
+          </>)}
+        </div>
+        <div className="navbar-end"></div>
+        </div>
   
- }
-};
-
-export default Header;
+         
+  
+        </nav>
+       ) 
+        
+        
+    
+   }
+  };
+  
+  export default Header;*/
